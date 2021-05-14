b30ac6d7e9b935506bf458731ebf61c29ada5f57
Danil Drozdov <danilka.drozdov2004@gmail.com>
2021-05-14 18:53:25 +0300

    Add part of ads

---
+++ Assets/Scripts/Service/AdvertismentController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class AdvertismentController : MonoBehaviour
+{
+    private static AdvertismentController Instance;
+
+    private void Awake() {
+        Instance = this;
+        Advertisement.Initialize("4128495", false);
+        StartCoroutine(ShowBannerWhenReady());
+    }
+
+    public static AdvertismentController GetInstance() {
+        return Instance;
+    }
+
+    private IEnumerator ShowBannerWhenReady() {
+        while(!Advertisement.IsReady("bannerPlacement")) {
+            yield return new WaitForSeconds(0.5f);
+        }
+        Advertisement.Banner.SetPosition(BannerPosition.BOTTOM_CENTER);
+        Advertisement.Banner.Show("bannerPlacement");
+    }
+}
